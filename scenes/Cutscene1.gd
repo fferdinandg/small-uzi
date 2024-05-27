36e8f34ab97c4e02d3220095d4079421a0cf3404
unknown <frferdinand22@gmail.com>
2024-05-27 13:57:32 +0700

    implement splashscreen

---
+++ scenes/Cutscene1.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+onready var timer = $Timer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	timer.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	Transition.transition("res://scenes/Cutscene2.tscn")
